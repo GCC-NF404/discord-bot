4971b3be59fb480c41805a487a3eeb53d91179ab
T.Kaga <36390388+Daikonnbatake@users.noreply.github.com>
2020-10-13 00:25:45 +0900

    マニュアル追加に伴い、コメントを削除しました

--- main.py
+++ main.py
@@ -1,34 +1,23 @@
 from discord.ext import commands
 import os,discord,logging
 
-# 定数の定義
-
 BOT = commands.Bot(command_prefix='$',help_command=None)
-CWD = str(__file__)[:-7] # DISCORD-BOT ディレクトリの絶対パス(末尾に/を含む)
-COLOR = 0xdddddd # 埋め込みの色
+CWD = str(__file__)[:-7]
+COLOR = 0xdddddd
+with open(CWD+'meta/admin.txt','r',encoding='utf-8') as f: ADMINS = f.read().split('\n')
+with open(CWD+'meta/token.txt','r',encoding='utf-8') as f: TOKEN = f.read()
 
-with open(CWD+'meta/admin.txt','r',encoding='utf-8') as f:
-    ADMINS = f.read().split('\n')
-
-with open(CWD+'meta/token.txt','r',encoding='utf-8') as f:
-    TOKEN = f.read()
-
-# コグを自動で取得する。システム関連は後々systemクラスとかで別ファイルに分けるかもしれない。
-def load_cogs(cwd):
+def load_cogs(cog_dir):
     logging.info('コグの読み込みを開始します。')
-    cogs = os.listdir(CWD+'cog')
+    cogs = os.listdir(cog_dir+'cog')
     for cog in cogs:
         if cog[len(cog)-3:]=='.py':
             BOT.load_extension('cog.'+str(cog[:-3]))
     logging.info('コグの読み込みが完了しました。')
 
-# botのイベント
-
 @BOT.event
-async def on_ready():
-    logging.info('botが起動しました。')
+async def on_ready(): logging.info('botが起動しました。')
 
-# エラーハンドリング
 @BOT.event
 async def on_command_error(ctx,error):
     channel = discord.utils.get(ctx.guild.channels, name='bot-log')
@@ -38,8 +27,6 @@ async def on_command_error(ctx,error):
     embed.add_field(name='発生エラー', value='```'+str(error)+'```', inline=False)
     if channel is not None:
         await channel.send(embed=embed)
-
-# botの実行
 
 load_cogs(CWD)
 BOT.run(TOKEN)
