917cf46367c2eb4183d111293ea57266a6bbb50a
T.Kaga <36390388+Daikonnbatake@users.noreply.github.com>
2020-10-12 23:15:10 +0900

    エラーハンドリングの下準備をしました。

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from discord.ext import commands
 import os,discord,logging
 
-
 # 定数の定義
 
 BOT = commands.Bot(command_prefix='$',help_command=None)
@@ -15,16 +14,25 @@ with open(CWD+'meta/token.txt','r',encoding='utf-8') as f:
 
 # コグを自動で取得する。システム関連は後々systemクラスとかで別ファイルに分けるかもしれない。
 def load_cogs(cwd):
+    logging.info('コグの読み込みを開始します。')
     cogs = os.listdir(CWD+'cog')
     for cog in cogs:
         if cog[len(cog)-3:]=='.py':
             BOT.load_extension('cog.'+str(cog[:-3]))
+    logging.info('コグの読み込みが完了しました。')
 
 # botのイベント
 
 @BOT.event
 async def on_ready():
-    logging.info('botが起動しました')
+    logging.info('botが起動しました。')
+
+# エラーハンドリング
+@BOT.event
+async def on_command_error(ctx,error):
+    channel = discord.utils.get(ctx.guild.channels, name="bot-log")
+    if channel is not None:
+        await channel.send("エラー")
 
 # botの実行
 
