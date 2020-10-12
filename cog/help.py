bfe3a8fc80a8bb3f9cc07395d09e179be9fb58a6
daikonn <daikonn@debian.debianlaptop.home>
2020-10-12 16:15:50 +0900

    helpコグにtodo-botからメッセージ送信部分を移植するなど

--- cog/help.py
+++ cog/help.py
@@ -1,16 +1,30 @@
 from discord.ext import commands
 import discord,json,logging
 
-CWD = str(__file__)[:-12]
+CWD = str(__file__)[:-11]
+#with open(CWD+'meta/embed.json','r',encoding='utf-8') as f:
+#    j = json.load(f)
+COLOR = 0xdddddd
 
 class Help(commands.Cog):
-    def __init__(self):
+    def __init__(self,ctx):
         with open(CWD+'meta/help.json','r',encoding='utf-8') as f:
             self.help=json.load(f)
             
     # Embedを返す
     def gen_help(self,title,description):
-        return discord.Embed(title=title,color=0xdddddd,description=description)
+        return discord.Embed(title=title,color=COLOR,description=description)
+    
+    @commands.command()
+    async def help(self,ctx,*args):
+        if len(args) and args[0]!='help':
+            if args[0] in self.help.keys():
+                await ctx.send(embed=self.genHelp('ヘルプ:'+args[0],self.help[args[0]]))
+            else:
+                await ctx.send(embed=self.genHelp('[エラー]','コマンド`'+args[0]+'`のヘルプは存在しません'))
+        else:
+            embed = discord.Embed(title='ヘルプ',description='接頭辞「$」の後に各コマンドを入力して使用します。\n各コマンドの詳細は「$help [コマンド]」で確認する事が出来ます。',color=COLOR)
+            await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Help(bot))
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from discord.ext import commands
 import os,discord,logging
 
-BOT = commands.Bot(command_prefix='$')
+
+# 定数の定義
+
+BOT = commands.Bot(command_prefix='$',help_command=None)
 CWD = str(__file__)[:-7] # DISCORD-BOT ディレクトリの絶対パス(末尾に/を含む)
 
 with open(CWD+'meta/admin.txt','r') as f:
@@ -10,15 +13,20 @@ with open(CWD+'meta/admin.txt','r') as f:
 with open(CWD+'meta/token.txt','r') as f:
     TOKEN = f.read()
 
+# コグを自動で取得する。システム関連は後々systemクラスとかで別ファイルに分けるかもしれない。
 def load_cogs(cwd):
     cogs = os.listdir(CWD+'cog')
     for cog in cogs:
         if cog[len(cog)-3:]=='.py':
             BOT.load_extension('cog.'+str(cog[:-3]))
 
+# botのイベント
+
 @BOT.event
 async def on_ready():
     logging.info('botが起動しました')
+
+# botの実行
 
 load_cogs(CWD)
 BOT.run(TOKEN)
