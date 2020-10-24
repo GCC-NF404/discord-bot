f15a3e7790ae4a5df0b91bf7b8cf5a014a80b752
T.Kaga <36390388+Daikonnbatake@users.noreply.github.com>
2020-10-24 14:17:39 +0900

    bot起動スクリプト経由で再起動するように変更

--- cog/system-c.py
+++ cog/system-c.py
@@ -17,8 +17,7 @@ class System(commands.Cog):
             channel = BOTSYS.is_exist_logch(ctx)
             if channel is not None:
                 await ctx.send('```botを再起動します。```')
-            subprocess.call('git pull origin master'.sprit())
-            os.execl(sys.executable, 'python3', '-B', str(__file__)[:-16]+'/main.py')
+            os.execl(sys.executable, 'sh', str(__file__)[:-28]+'/bot.sh')
         else:
             await ctx.send(BOTSYS.pError)
 
